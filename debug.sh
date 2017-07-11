4a234c72dc0e2fe90c0925c366c3eef51cf47baf
Teva GASCOIN <cbind@bk.ru>
2017-07-11 14:36:44 +0200

    removing old version

--- debug.sh
+++
@@ -1,1 +0,0 @@
-clang -g3 -fsanitize=address -Wall -Werror -Wextra -ltermcap srcs/builtins/*.c srcs/shell/*.c srcs/engine/*.c srcs/env/*.c libft/*.c srcs/vm/*.c srcs/ast/*.c srcs/lexer/*.c -I includes/ -O srcs/main.c && ASAN_SYMBOLIZER_PATH=/usr/lib/llvm-4.0/bin/llvm-symbolizer ./a.out
--- srcs/lexer/lx_remove_uslesstokens.c
+++
@@ -1,6 +0,0 @@
-#include "lexer.h"
-
-void	lx_remove_uslesstoken(t_token **tokens)
-{
-	(void)tokens;
-}
