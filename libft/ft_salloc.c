9f38061662f4ec94c871c81e02891af2e69a57da
Teva GASCOIN <cbind@bk.ru>
2017-11-24 13:50:02 +0100

    removing realloc

--- libft/ft_salloc.c
+++ libft/ft_salloc.c
@@ -6,7 +6,7 @@
 /*   By: tgascoin <tgascoin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/14 16:45:58 by tgascoin          #+#    #+#             */
-/*   Updated: 2017/11/14 16:46:03 by tgascoin         ###   ########.fr       */
+/*   Updated: 2017/11/24 13:49:36 by tgascoin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,5 @@ void	*ft_scalloc(size_t size)
 	if (!(ret = malloc(size)))
 		exit(1);
 	ft_bzero(ret, size);
-	return (ret);
-}
-
-void	*ft_srealloc(void *ptr, size_t size)
-{
-	void	*ret;
-
-	if (!(ret = realloc(ptr, size)))
-		exit(1);
 	return (ret);
 }
